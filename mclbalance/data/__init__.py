5b823bcd2b6f1df7d3886d8be7116be0467228dc
oKatanaaa <whitemarsstudios@gmail.com>
2021-02-02 17:53:47 +0400

    fix: load_data

--- mclbalance/data/__init__.py
+++ mclbalance/data/__init__.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import pathlib
+import os
 
 
 def load_data():
-    df = pd.read_csv('uniq_hvc.csv')
+    data_dir_path = pathlib.Path(__file__).parent.absolute()
+    data_path = os.path.join(data_dir_path, 'uniq_hvc.csv')
+    df = pd.read_csv(data_path)
     return df.to_numpy()[:, 1:]
 
 
